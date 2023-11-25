c6a0e7f64d426d0e225f28da2be7dee6ecaab292
root <te19980@163.com>
2023-11-25 16:17:22 +0800

    init

--- boot/bootsect.s
+++ boot/bootsect.s
@@ -70,7 +70,7 @@ load_setup:
 	mov	dx,#0x0000		! drive 0, head 0
 	mov	cx,#0x0002		! sector 2, track 0
 	mov	bx,#0x0200		! address = 512, in INITSEG
-	mov	ax,#0x0200+SETUPLEN	! service 2, nr of sectors//
+	mov	ax,#0x0200+SETUPLEN	! service 2, nr of sectors
 	int	0x13			! read it
 	jnc	ok_load_setup		! ok - continue
 	mov	dx,#0x0000
